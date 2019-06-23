b4ad8113677586a9d3f3ae72d272194123c28a7a
Zhujun Xiao <zhujunxiao@Zhujuns-MacBook-Pro-2.local>
2019-06-23 15:30:49 -0700

    add comments

--- new_video/generate_ground_truth.sh
+++ new_video/generate_ground_truth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 declare -A RESOL
 declare -A NUM_OF_FRAMES
+# Add resolution of new dataset
 RESOL=([walking]="3840,2160" \
 	   [driving_downtown]="3840,2160" \
 	   [highway]="1280,720" \
@@ -16,6 +17,7 @@ RESOL=([walking]="3840,2160" \
 	   [cropped_crossroad3]="600,400" \
 	   [cropped_driving2]="600,400")
 
+# Add number of frames of new dataset
 NUM_OF_FRAMES=([walking]=68300\
 	   [driving_downtown]=32000 \
 	   [highway]=51200 \
@@ -32,30 +34,20 @@ NUM_OF_FRAMES=([walking]=68300\
 	   [cropped_driving2]=39000)
 
 
+
+# Change CODE_PATH to the object detection path.
 CODE_PATH="/home/zhujun/video_analytics_pipelines/models/research/object_detection"
-
-
-
-
-# # FULL_MODEL_PATH="/home/zhujun/video_analytics_pipelines/models/research/"\
-# # "object_detection/faster_rcnn_resnet101_kitti_2018_01_28/"\
-# # "frozen_inference_graph.pb"
-
-
+# Change full model path to path where trained object detection model is saved.
 FULL_MODEL_PATH="/home/zhujun/video_analytics_pipelines/models/research/"\
 "object_detection/faster_rcnn_resnet101_coco_2018_01_28/"\
 "frozen_inference_graph.pb"
 
-# #"object_detection/faster_rcnn_resnet101_coco_2018_01_28/"
-
-# "object_detection/ssd_mobilenet_v2_coco_2018_03_29/"\
-
-# DATASET_LIST="highway driving_downtown crossroad crossroad2 crossroad3 crossroad4 crossroad5 crossroad6 crossroad7 walking driving1 driving2"\
-# " cropped_crossroad3 cropped_driving2"
+# Change DATA_PATH to downloaded Youtube video path
 DATA_PATH='/home/zhujun/video_analytics_pipelines/dataset/Youtube/'
-DATASET_LIST="crossroad2 crossroad4 crossroad5"
+# Change dataset_list to your dataset name
+DATASET_LIST="crossroad2"
+# Choose an idle GPU
 GPU="1"
-RESIZE_RESOL=540p
 
 
 
@@ -63,20 +55,13 @@ for DATASET_NAME in $DATASET_LIST
 do
 echo ${DATASET_NAME} 
 
-python3 resize.py \
-		--dataset=${DATASET_NAME} \
-		--frame_count=${NUM_OF_FRAMES[${DATASET_NAME}]} \
-		--resize_resol=$RESIZE_RESOL \
-		--path=$DATA_PATH
 
-echo 'Done resizing.'
 python3 ${CODE_PATH}/dataset_tools/create_youtube_video_input.py \
 		--dataset=${DATASET_NAME} \
 		--resol=${RESOL[${DATASET_NAME}]} \
 		--frame_count=${NUM_OF_FRAMES[${DATASET_NAME}]} \
-		--resize=True \
+		--resize=False \
 		--path=$DATA_PATH \
-		--resize_resol=$RESIZE_RESOL
 
 echo "Done creating input!"
 python3  ${CODE_PATH}/inference/infer_detections_for_ground_truth.py \
@@ -84,21 +69,19 @@ python3  ${CODE_PATH}/inference/infer_detections_for_ground_truth.py \
 		--discard_image_pixels \
 		--dataset=${DATASET_NAME} \
 		--gpu=$GPU \
-		--resize=True \
+		--resize=False \
 		--path=$DATA_PATH \
-		--resize_resol=$RESIZE_RESOL
-# # do
+
 python3 infer_object_id_youtube.py \
 		--dataset=${DATASET_NAME} \
 		--path=$DATA_PATH \
-		--resize=True \
+		--resize=False \
 		--resol=${RESOL[${DATASET_NAME}]} \
-		--resize_resol=$RESIZE_RESOL
 
-# python3 ../dataset_profile/video_feature_youtube.py \
-# 		--dataset=${DATASET_NAME} \
-# 		--path=$DATA_PATH \
-# 		--resol=${RESOL[${DATASET_NAME}]} 
+python3 video_feature_youtube.py \
+		--dataset=${DATASET_NAME} \
+		--path=$DATA_PATH \
+		--resol=${RESOL[${DATASET_NAME}]} 
 
 done
 
