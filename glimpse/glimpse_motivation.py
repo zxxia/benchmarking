7a4452f12d634e137e251a38fec860668b41f352
Zhengxu Xia <kevinxzx95@gmail.com>
2019-07-14 17:23:14 -0500

    Added motivation figure drawing script and modified motivation scipts a little bit

--- glimpse/glimpse_motivation.py
+++ glimpse/glimpse_motivation.py
@@ -6,26 +6,29 @@ import os
 import json
 from glimpse_kitti import pipeline, compute_target_frame_rate
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from my_utils import load_metadata
 kitti = False
-DATASET_LIST =  ['tw_road']#'jp','russia1', 'park']#['traffic', 'highway_normal_traffic', 'highway_no_traffic', 
+DATASET_LIST =  ['highway_no_traffic']#'jp','russia1', 'park']#['traffic', 'highway_normal_traffic', 'highway_no_traffic', 
                 #'reckless_driving', 'motor', 'russia', 'jp_hw', 'tw_road', 
                 #'tw_under_bridge']
 PROFILE_LENGTH = 30 # 30 seconds
-OFFSET = 0 # offset from the start of the video in unit of second
+OFFSET = 1 # offset from the start of the video in unit of second
 # standard_frame_rate = 10.0 # for comparison with KITTI
 PATH = '/mnt/data/zhujun/new_video/'
-SHORT_VIDEO_LENGTH = 2*60
+SHORT_VIDEO_LENGTH = int(1*60)
 INFERENCE_TIME = 100 # avg. GPU processing  time
 TARGET_F1 = 0.9
 
 # Two parameters: frame difference threshold, tracking error thresh
-para1_list = [5,7,8]#[1.5,2,3,4,5] #[1,2,5,10]#
-para2_list = [2.5,3,5]#[2.2,2.3,2.45,2.5,2.6] # [0.5,0.7,1,3,10]
- 
-
+para1_list = [2,2.5,3,3.5,4,4.5,5,] # [2,3,4,5] #[1,2,5,10]#
+para2_list = [15,11,10,8,5,4,3.5,3.1,3,1,0.6,0.5,0.3,0.1,0.05]
+# [2.2,2.3,2.5,2.6] # [0.5,0.7,1,3,10]
+#[2,3,5,10,] for russia1
+#[0.5,0.55,0.6,0.7,]
+# [10,12,14,15,20,25] for drift
+# [1,1.5,1.8,2,3,4]
 def get_gt_dt(annot_path, height):
     # read ground truth and full model (server side) detection results
     gt_annot = defaultdict(list)
@@ -57,8 +60,6 @@ def get_gt_dt(annot_path, height):
 
 def main():
     # final_result_f = open('youtube_glimpse_motivation.csv','w')
-    final_result_f = open('glimpse_motivation_result.csv','w')
-    final_result_f.write('video chunk,para1,para2,f1,frame rate\n')
     # choose the first 3 mins to get the best frame diff thresh
     for video_type in DATASET_LIST:
         metadata = load_metadata(PATH + video_type + '/metadata.json')
@@ -76,88 +77,88 @@ def main():
         img_path = PATH + video_type +'/'
         gt_annot, dt_annot, frame_end = get_gt_dt(annot_path, height)
 
-        num_of_short_videos = frame_end//(SHORT_VIDEO_LENGTH*frame_rate)
+        num_of_short_videos = (frame_end - OFFSET * frame_rate)//(SHORT_VIDEO_LENGTH*frame_rate)
+        
+        test_f1_list = [] # A list of f1 scores computed using the best config over the entire video
+        test_fps_list = [] # A list of fps computed over the entire video using the best config
 
-        for i in range(num_of_short_videos):
-            start = i * (SHORT_VIDEO_LENGTH*frame_rate) +1
+        with open('glimpse_motivation_result_{}.csv'.format(video_type), 'w') as final_result_f:
+            final_result_f.write('video chunk,para1,para2,f1,frame rate\n')
+            for i in range(num_of_short_videos):
+                start = i * (SHORT_VIDEO_LENGTH*frame_rate) + 1 + OFFSET * frame_rate
 
-            end = (i+1) * (SHORT_VIDEO_LENGTH*frame_rate)
-            profile_start = start
-            profile_end = start + PROFILE_LENGTH*frame_rate
-            print("short video start={}, end={}".format(start, end))
-            print("profile start={}, end={}".format(profile_start, profile_end))
-            print('profiling short video {}'.format(i))
-            # print(video_type, seg_index)
-            # Run inference on the first 30s video
-            frame_rate_list = []
-            f1_list = []
-            min_f1_gt_target = 1.0 # the minimum f1 score which is greater than
-                                   # or equal to target f1(e.g. 0.9)
-            best_para1 = -1
-            best_para2 = -1
-            test_f1_list = [] # A list of f1 scores computed using the best config over the entire video
-            test_fps_list = [] # A list of fps computed over the entire video using the best config
-            for para1 in para1_list:
-                for para2 in para2_list:
-                    csvf = open('no_meaning.csv','w')
-                    # larger para1, smaller thresh, easier to be triggered
-                    frame_difference_thresh = image_resolution[0]*image_resolution[1]/para1
-                    tracking_error_thresh = para2
-                    # images start from index 1
-                    print(img_path, profile_start, profile_end) 
-                    triggered_frame, f1 = pipeline(img_path, dt_annot, gt_annot,
-                                                   profile_start, profile_end, csvf, 
-                                                   image_resolution, frame_rate, 
-                                                   frame_difference_thresh, 
-                                                   tracking_error_thresh, False)
+                end = (i + 1) * (SHORT_VIDEO_LENGTH*frame_rate) + OFFSET * frame_rate
+                profile_start = start
+                profile_end = start + PROFILE_LENGTH*frame_rate
+                print("short video start={}, end={}".format(start, end))
+                print("profile start={}, end={}".format(profile_start, profile_end))
+                print('profiling short video {}'.format(i))
+                # print(video_type, seg_index)
+                # Run inference on the first 30s video
+                frame_rate_list = []
+                f1_list = []
+                min_f1_gt_target = 1.0 # the minimum f1 score which is greater than
+                                       # or equal to target f1(e.g. 0.9)
+                min_fps = frame_rate
 
-                    current_frame_rate = triggered_frame / float(PROFILE_LENGTH)
-                    frame_rate_list.append(current_frame_rate)
-                    f1_list.append(f1)
+                best_para1 = -1
+                best_para2 = -1
+                is_tuning = True
+                for para1 in para1_list:
+                    if not is_tuning:
+                        break
+                    for para2 in para2_list:
+                        if not is_tuning:
+                            break
+                        csvf = open('no_meaning.csv','w')
+                        # larger para1, smaller thresh, easier to be triggered
+                        frame_difference_thresh = image_resolution[0]*image_resolution[1]/para1
+                        tracking_error_thresh = para2
+                        # images start from index 1
+                        print(img_path, profile_start, profile_end) 
+                        triggered_frame, f1 = pipeline(img_path, dt_annot, gt_annot,
+                                                       profile_start, profile_end, csvf, 
+                                                       image_resolution, frame_rate, 
+                                                       frame_difference_thresh, 
+                                                       tracking_error_thresh, False)
 
-                    if f1 >= TARGET_F1 and f1 < min_f1_gt_target:
-                        # record min f1 which is greater than target f1
-                        min_f1_gt_target = f1
-                        # record the best config
-                        best_para1 = para1
-                        best_para2 = para2
+                        current_frame_rate = triggered_frame / float(PROFILE_LENGTH)
+                        frame_rate_list.append(current_frame_rate)
+                        f1_list.append(f1)
 
-                    print('Profiled f1 = {}, Profiled frame rate = {}'.format(f1, current_frame_rate))
-            # if max(f1_list) < TARGET_F1:
-            #     para1 = 20
-            # else:
-            #     index = next(x[0] for x in enumerate(f1_list) if x[1] >= TARGET_F1)
-            #     para1 = para1_list[index]
+                        if f1 >= TARGET_F1 and f1 <=min_f1_gt_target  and current_frame_rate < min_fps:
+                            # record min f1 which is greater than target f1
+                            min_f1_gt_target = f1
+                            min_fps = current_frame_rate
+                            # record the best config
+                            best_para1 = para1
+                            best_para2 = para2
+                            is_tuning = False 
 
+                        print('para1 = {}, para2 = {}, Profiled f1 = {}, Profiled frame rate = {}'.format(para1, para2, f1, current_frame_rate))
 
-            print("Finish profiling on the {} 30s segment...".format(i))
+                print("Finish profiling on the {} {}s segment...".format(i, PROFILE_LENGTH))
+                print("best_para1 = {}, best_para2={}".format(best_para1, best_para2))
+                print("Start testing....")
+                # use the selected parameters for the next 5 mins
+                frame_difference_thresh = image_resolution[0]*image_resolution[1]/best_para1   
+                tracking_error_thresh = best_para2
 
-            print("Start testing...")
-            # use the selected parameters for the next 5 mins
-            frame_difference_thresh = image_resolution[0]*image_resolution[1]/best_para1   
-            tracking_error_thresh = best_para2
-
-
-
-            if end > frame_count:
-                end = frame_count
-
-            assert(start>=1 and start <= frame_count)
-            assert(end>=1 and end <= frame_count)
-            csvf = open('no_meaning.csv','w')
-            triggered_frame, f1 = pipeline(img_path, dt_annot, gt_annot, start,
-                                           end, csvf, image_resolution,
-                                           frame_rate, frame_difference_thresh, 
-                                           tracking_error_thresh, False)
-            print("triggered {} frames over {} seconds".format(triggered_frame, SHORT_VIDEO_LENGTH))
-            fps = float(triggered_frame)/float(SHORT_VIDEO_LENGTH)
-            test_f1_list.append(f1)
-            test_fps_list.append(fps)
-            print('F1, current_frame_rate:', f1, fps)
-            final_result_f.write(video_type + '_' + str(i) + ',' + 
-                  str(best_para1) + ',' + str(best_para2) + ',' +
-                  str(f1) + ',' + str(fps) + '\n')
-            print("Finished testing...")
+                assert(end>=1 and end <= frame_count)
+                csvf = open('no_meaning.csv','w')
+                triggered_frame, f1 = pipeline(img_path, dt_annot, gt_annot, start,
+                                               end, csvf, image_resolution,
+                                               frame_rate, frame_difference_thresh, 
+                                               tracking_error_thresh, False)
+                print("triggered {} frames over {} seconds".format(triggered_frame, SHORT_VIDEO_LENGTH))
+                fps = float(triggered_frame)/float(SHORT_VIDEO_LENGTH)
+                test_f1_list.append(f1)
+                test_fps_list.append(fps)
+                print('tested F1, tested frame_rate:', f1, fps)
+                final_result_f.write(video_type + '_' + str(i) + ',' + 
+                      str(best_para1) + ',' + str(best_para2) + ',' +
+                      str(f1) + ',' + str(fps/frame_rate) + '\n')
+                print("Finished testing...")
 
         test_relative_fps_list = [x/frame_rate for x in test_fps_list]
         if test_relative_fps_list and test_f1_list:
@@ -170,7 +171,7 @@ def main():
     plt.legend()
     plt.savefig("/home/zxxia/figs/glimpse/glimpse_motivation.png")
     #plt.clf()
-    plt.show()
+    #plt.show()
 
 
 if __name__=='__main__':
--- videostorm/videostorm_motivation.py
+++ videostorm/videostorm_motivation.py
@@ -8,11 +8,11 @@ from my_utils import interpolation, load_metadata, compute_f1
 PATH  = '/mnt/data/zhujun/new_video/'
 TEMPORAL_SAMPLING_LIST = [20,15,10,5,4,3,2.5,2,1.8,1.5,1.2,1]
 MODEL_LIST = ['FasterRCNN'] # MODEL_LIST = ['FasterRCNN','SSD']
-DATASET_LIST = ['tw_road']
+DATASET_LIST = ['highway_no_traffic']
 IOU_THRESH = 0.5
 TARGET_F1 = 0.9
-OFFSET = 0 # The time offset from the start of the video. Unit: seconds
-CHUNK_LENGTH = 2*60 # A long video is chopped into chunks. Unit: second
+OFFSET = 1 # The time offset from the start of the video. Unit: seconds
+CHUNK_LENGTH = int(1*60) # A long video is chopped into chunks. Unit: second
 PROFILE_LENGTH = 30 # Profiling length within a chunk. Unit: second
 
 def profile(dataset, frame_rate, gt, start_frame, chunk_length=30):
@@ -162,7 +162,6 @@ def profile_eval(dataset, frame_rate, gt, best_model, best_sample_rate,
 def main():
 
     # short_video_length = 5*60 # divide each video into 5-min
-    f = open('VideoStorm_motivation_final.csv','w')
     for dataset in DATASET_LIST:
         print("processing", dataset)
         metadata = load_metadata(PATH + dataset + '/metadata.json')
@@ -177,34 +176,35 @@ def main():
        
         # Chop long videos into small chunks
         # Floor division drops the last sequence of frames which is not as long as CHUNK_LENGTH
-        num_of_chunks = num_of_frames // (CHUNK_LENGTH * frame_rate)
+        num_of_chunks = (num_of_frames - OFFSET * frame_rate)// (CHUNK_LENGTH * frame_rate)
         
 
         
         test_f1_list = list()
         test_fps_list = list()
 
-        for i in range(num_of_chunks): 
-            # the 1st frame in the chunk
-            start_frame = i * (CHUNK_LENGTH * frame_rate) + 1 #+ OFFSET * frame_rate
-            # the last frame in teh chunk
-            end_frame = (i+1) * (CHUNK_LENGTH * frame_rate) #+ OFFSET * frame_rate
-            print('short video start={}, end={}'.format(start_frame, end_frame)) 
-            # profile the first PROFILE_LENGTH seconds of the chunk
-            assert(CHUNK_LENGTH > PROFILE_LENGTH)
-            best_model, best_frame_rate = profile(dataset, frame_rate, gt, 
-                                                  start_frame, PROFILE_LENGTH)
-            # test on the whole video
-            best_sample_rate = standard_frame_rate / best_frame_rate
+        with open('videostorm_motivation_result_{}.csv'.format(dataset),'w') as f:
+            for i in range(num_of_chunks): 
+                # the 1st frame in the chunk
+                start_frame = i * (CHUNK_LENGTH * frame_rate) + 1 + OFFSET * frame_rate
+                # the last frame in teh chunk
+                end_frame = (i+1) * (CHUNK_LENGTH * frame_rate) + OFFSET * frame_rate
+                print('short video start={}, end={}'.format(start_frame, end_frame)) 
+                # profile the first PROFILE_LENGTH seconds of the chunk
+                assert(CHUNK_LENGTH > PROFILE_LENGTH)
+                best_model, best_frame_rate = profile(dataset, frame_rate, gt, 
+                                                      start_frame, PROFILE_LENGTH)
+                # test on the whole video
+                best_sample_rate = standard_frame_rate / best_frame_rate
 
-            f1 = profile_eval(dataset, frame_rate, gt, best_model, 
-                              best_sample_rate, start_frame+PROFILE_LENGTH * frame_rate, end_frame)
+                f1 = profile_eval(dataset, frame_rate, gt, best_model, 
+                                  best_sample_rate, start_frame+PROFILE_LENGTH * frame_rate, end_frame)
 
-            test_f1_list.append(f1)
-            test_fps_list.append(best_frame_rate)
-            print(dataset+str(i), best_frame_rate, f1)
-            f.write(dataset + '_' + str(i) + ',' + str(best_model) + ',' + 
-                    str(f1) + ',' + str(best_frame_rate/standard_frame_rate) + '\n')
+                test_f1_list.append(f1)
+                test_fps_list.append(best_frame_rate)
+                print(dataset+str(i), best_frame_rate, f1)
+                f.write(dataset + '_' + str(i) + ',' + str(best_model) + ',' + 
+                        str(f1) + ',' + str(best_frame_rate/standard_frame_rate) + '\n')
         test_relative_fps_list = [x/standard_frame_rate for x in test_fps_list]
         if test_relative_fps_list and test_f1_list:
             plt.scatter(test_relative_fps_list, test_f1_list, label=dataset)
