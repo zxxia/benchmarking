ace148d9e216a7f69255f38fc5d36e9f93557965
Zhengxu Xia <kevinxzx95@gmail.com>
2019-09-06 14:49:04 -0500

    HotCloud SimpleProto Pipeline

--- SimpleProto/SimpleProto.py
+++ SimpleProto/SimpleProto.py
@@ -10,57 +10,60 @@ PATH = '/mnt/data/zhujun/dataset/Youtube/'
 DATASET_LIST = ['motorway']
 ALPHA = 0.2
 BETA = 0.4
-OFFSET = 2*60# + 30
+OFFSET = 2*60  # + 30
 TEST_LENGTH = 30
 RESOLUTION_LIST = ['480p', '540p', '720p']
-RESOLUTION_DICT = {'360p': (640,360),
-              '480p': (854,480),
-              '540p': (960,540),
-              '576p': (1024,576),
-              '720p': (1280,720),
-              '1080p': (1920,1080),
-              'original': (1280, 720)
-             }
+RESOLUTION_DICT = {'360p': (640, 360),
+                   '480p': (854, 480),
+                   '540p': (960, 540),
+                   '576p': (1024, 576),
+                   '720p': (1280, 720),
+                   '1080p': (1920, 1080),
+                   'original': (1280, 720)}
 DEBUG = False
-#DEBUG = True
+# DEBUG = True
+
 
 def debug_print(msg):
     if DEBUG:
         print(msg)
 
-def crop_image(img_in, xmin, ymin, xmax, ymax, w_out, h_out, img_out):
-    cmd= ['ffmpeg',  '-y', '-hide_banner', '-i', img_in, '-vf',
-          'crop={}:{}:{}:{},pad={}:{}:0:0:black'.format(xmax-xmin, ymax-ymin,
-          xmin, ymin, w_out, h_out), img_out]
 
-    #print(cmd)
+def crop_image(img_in, xmin, ymin, xmax, ymax, w_out, h_out, img_out):
+    cmd = ['ffmpeg',  '-y', '-hide_banner', '-i', img_in, '-vf',
+           'crop={}:{}:{}:{},pad={}:{}:0:0:black'
+           .format(xmax-xmin, ymax-ymin, xmin, ymin, w_out, h_out), img_out]
+
+    # print(cmd)
     subprocess.run(cmd)
 
-    #pdb.set_trace()
-    #cmd = ['ffmpeg', '-y', '-vcodec', 'libx264',
-    #       '-crf', 25, '-pix_fmt', 'yuv420p', '-hide_banner', 'crop.mp4']
+    # pdb.set_trace()
+    # cmd = ['ffmpeg', '-y', '-vcodec', 'libx264',
+    #        '-crf', 25, '-pix_fmt', 'yuv420p', '-hide_banner', 'crop.mp4']
 
-    #size = os.path.getsize('crop.mp4')
-    #os.remove(img_out)
-    #return size
+    # size = os.path.getsize('crop.mp4')
+    # os.remove(img_out)
+    # return size
+
 
 def compress_images_to_video_v1(path: str, start_frame: int, nb_frames: int,
-                             frame_rate, resolution,
-                             quality: int, output_name: str):
+                                frame_rate, resolution,
+                                quality: int, output_name: str):
     '''
     Compress a set of frames to a video.
     start frame: input image frame start index
     '''
-    cmd = ['ffmpeg', '-r', str(frame_rate), '-f', 'image2', '-s', str(resolution),
-           '-start_number', str(start_frame), '-i', '{}%06d.jpg'.format(path),
-           '-vframes', str(nb_frames), '-vcodec', 'libx264', '-crf', str(quality),
-           '-pix_fmt', 'yuv420p', '-hide_banner', output_name]
-    subprocess.run(cmd)#, stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
+    cmd = ['ffmpeg', '-r', str(frame_rate), '-f', 'image2', '-s',
+           str(resolution), '-start_number', str(start_frame), '-i',
+           '{}%06d.jpg'.format(path), '-vframes', str(nb_frames), '-vcodec',
+           'libx264', '-crf', str(quality), '-pix_fmt', 'yuv420p',
+           '-hide_banner', output_name]
+    subprocess.run(cmd)
+    # , stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
 
     video_size = os.path.getsize(output_name)
     os.remove(output_name)
     return video_size
-
 
 
 def compress_images_to_video(list_file: str, frame_rate: str, resolution: str,
@@ -74,16 +77,17 @@ def compress_images_to_video(list_file: str, frame_rate: str, resolution: str,
            output_name]
     subprocess.run(cmd)
 
+
 def compute_video_size(img_path, start, end, target_frame_rate, frame_rate,
                        image_resolution):
-    #pdb.set_trace()
+    # pdb.set_trace()
     sample_rate = frame_rate/target_frame_rate
     # Create a tmp list file contains all the selected iamges
     tmp_list_file = 'list.txt'
     with open(tmp_list_file, 'w') as f:
         for img_index in range(start, end + 1):
             # based on sample rate, decide whether this frame is sampled
-            if img_index%sample_rate >= 1:
+            if img_index % sample_rate >= 1:
                 continue
             else:
                 line = 'file \'{}/{:06}.jpg\'\n'.format(img_path, img_index)
@@ -94,8 +98,9 @@ def compute_video_size(img_path, start, end, target_frame_rate, frame_rate,
                              25, 'tmp.mp4')
     video_size = os.path.getsize("tmp.mp4")
     os.remove('tmp.mp4')
-    #os.remove(tmp_list_file)
-    print('target frame rate={}, target image resolution={}. video size={}'.format(target_frame_rate, image_resolution, video_size))
+    # os.remove(tmp_list_file)
+    print('target frame rate={}, target image resolution={}. video size={}'
+          .format(target_frame_rate, image_resolution, video_size))
     return video_size
 
 
@@ -113,6 +118,7 @@ def scale(box, in_resol, out_resol):
     box[3] = int(box[3] * y_scale)
     return box
 
+
 def convert_box_coordinate(box):
     """
     box: x, y, w, h
@@ -124,24 +130,25 @@ def convert_box_coordinate(box):
     ymax = ymin + box[3]
     return [xmin, ymin, xmax, ymax]
 
+
 def overlap_percentage(boxA, boxB):
     """
     boxA, boxB: [xmin, ymin, xmax, ymax]
     """
-    #boxA = convert_box_coordinate(boxA)
-    #boxB = convert_box_coordinate(boxB)
+    # boxA = convert_box_coordinate(boxA)
+    # boxB = convert_box_coordinate(boxB)
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
     xB = min(boxA[2], boxB[2])
     yB = min(boxA[3], boxB[3])
 
-  # compute the area of intersection rectangle
+    # compute the area of intersection rectangle
     interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-  # compute the area of both the prediction and ground-truth
-  # rectangles
+    # compute the area of both the prediction and ground-truth
+    # rectangles
     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-    #print(boxA, boxB)
-    #print(xA, yA, xB, yB)
+    # print(boxA, boxB)
+    # print(xA, yA, xB, yB)
     return float(interArea)/boxBArea
 
 
@@ -152,15 +159,15 @@ def main():
         frame_cnt = metadata['frame count']
         frame_rate = metadata['frame rate']
         original_resol = str(resolution[1]) + 'p'
-        gt_file = PATH  + dataset + '/' + original_resol + '/profile/updated_gt_FasterRCNN_COCO.csv'
+        gt_file = PATH + dataset + '/' + original_resol + '/profile/updated_gt_FasterRCNN_COCO.csv'
         gt, nb_frames = load_full_model_detection_new(gt_file)
-        #nb_frames = 1800
+        # nb_frames = 1800
         print(len(gt), nb_frames)
         # pdb.set_trace()
         start_frame = 1 + OFFSET * frame_rate
-        #end_frame = start_frame + TEST_LENGTH * frame_rate - 1
+        # end_frame = start_frame + TEST_LENGTH * frame_rate - 1
         end_frame = start_frame + 240 - 1
-        original_bw = compute_video_size(PATH+dataset+'/'+original_resol +'/',
+        original_bw = compute_video_size(PATH+dataset+'/'+original_resol + '/',
                                          start_frame, end_frame, frame_rate,
                                          frame_rate, resolution)
 
