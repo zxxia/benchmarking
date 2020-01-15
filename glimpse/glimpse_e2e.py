cd3bf66472d46e22d65dd1dc994e3ee1b8435252
Zhengxu Xia <kevinxzx95@gmail.com>
2020-01-14 21:04:27 -0600

    Refactor glimpse code and add time measurement code.

--- glimpse/glimpse_e2e.py
+++ glimpse/glimpse_e2e.py
@@ -1,191 +1,154 @@
-# import matplotlib
-# matplotlib.use('Agg')
-from glimpse import pipeline  # , compute_target_frame_rate
-# from matplotlib import pyplot as plt
-from utils.model_utils import load_full_model_detection
-from utils.utils import load_metadata
 import argparse
-import json
+import csv
+import os
 import numpy as np
-display = True
-# kitti = False
-# DATASET_LIST = sorted(['highway', 'crossroad2', 'crossroad', 'crossroad3',
-#                        'crossroad4', 'driving1','driving2','traffic','jp_hw',
-#                        'russia', 'tw_road', 'tw_under_bridge','jp','russia1',
-#                        'highway_normal_traffic', 'tw', 'tw1', 'drift', 'nyc',
-#                        'motorway', 'park', 'lane_split', 'driving_downtown'])
+from benchmarking.glimpse.Glimpse import Glimpse
+from benchmarking.video import YoutubeVideo
 
-# PROFILE_LENGTH = 30  # 30 seconds
-# OFFSET = 0  # offset from the start of the video in unit of second
-# # standard_frame_rate = 10.0 # for comparison with KITTI
-# PATH = '/mnt/data/zhujun/dataset/Youtube/'
-# SHORT_VIDEO_LENGTH = 30
-# INFERENCE_TIME = 100 # avg. GPU processing  time
-# TARGET_F1 = 0.9
-PARA1_LIST = [200]  # [2,3,4,5,6,8,10,15] #[1,2,5,10]#
-PARA2_LIST = np.concatenate([np.arange(10, 1, -2), np.arange(1, 0, -0.1)])
+PARA1_LIST_DICT = {
+    'crossroad': np.arange(30, 42, 2),
+    'crossroad2': np.arange(20, 42, 2),
+    'crossroad3': np.arange(70, 100, 3),
+    'crossroad4': np.arange(30, 62, 2),
+    'drift': np.arange(290, 400, 10),
+    'driving1': np.arange(10, 25, 2),
+    'driving2': np.arange(5, 30, 2),
+    'driving_downtown': np.arange(4, 20, 2),
+    'highway': np.arange(30, 40, 1),
+    'highway_normal_traffic': np.arange(34, 40, 2),
+    'jp': np.arange(30, 40, 2),
+    'jp_hw': np.arange(30, 40, 2),
+    'lane_split': np.arange(6, 14, 2),
+    'motorway': np.arange(2, 6, 2),
+    'nyc': np.arange(2, 22, 2),
+    'park': np.arange(2, 20, 2),
+    'russia': np.arange(100, 400, 20),
+    'russia1': np.arange(100, 400, 20),
+    'traffic': np.arange(6, 15, 1),
+    'tw': np.arange(25, 55, 5),
+    'tw1': np.arange(25, 55, 5),
+    # 'tw_road': np.arange(15, 45, 5),
+    'tw_under_bridge': np.arange(350, 450, 10),
+}
+PARA2_LIST = [10, 8, 5, 3, 2, 1]
+DET_ROOT = '/data/zxxia/benchmarking/results/videos'
+IMG_ROOT = '/data/zxxia/videos'
+
+
+def parse_args():
+    """Parse the input arguments required by the code."""
+    parser = argparse.ArgumentParser(
+        description="Glimpse with perfect tracking")
+    # parser.add_argument("--path", type=str, required=True,
+    #                     help="path contains all datasets")
+    parser.add_argument("--video", type=str, required=True,
+                        help="video name")
+    parser.add_argument("--metadata", type=str,  # required=True,
+                        help="metadata file in Json")
+    parser.add_argument("--output", type=str, required=True,
+                        help="output result file")
+    parser.add_argument("--log", type=str,
+                        help="profiling log file")
+    parser.add_argument("--short_video_length", type=int, required=True,
+                        help="short video length in seconds")
+    parser.add_argument("--profile_length", type=int, required=True,
+                        help="profile length in seconds")
+    parser.add_argument("--offset", type=int, default=0,
+                        help="offset from beginning of the video in seconds")
+    parser.add_argument("--trace_path", type=str,
+                        help="trace path contains all traces")
+    parser.add_argument("--profile_trace_path", type=str,
+                        help="profile trace path contains all profile traces")
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Glimpse with perfect tracking")
-    parser.add_argument("--path", type=str,
-                        help="path contains all datasets")
-    parser.add_argument("--video", type=str,
-                        help="video name")
-    parser.add_argument("--metadata", type=str, default='',
-                        help="metadata file in Json")
-    parser.add_argument("--output", type=str,
-                        help="output result file")
-    parser.add_argument("--short_video_length", type=int,
-                        help="short video length in seconds")
-    parser.add_argument("--profile_length", type=int,
-                        help="profile length in seconds")
-    parser.add_argument("--offset", type=int,
-                        help="offset from beginning of the video in seconds")
-    parser.add_argument("--target_f1", type=float, help="target F1 score")
-    parser.add_argument("--fps", type=int, default=0, help="frame rate")
-    parser.add_argument("--resolution", nargs='+', type=int,
-                        default=[], action='store', help="video resolution")
-    parser.add_argument("--format", type=str, default='{:06d}.jpg',
-                        help="image name format")
+    args = parse_args()
 
-    args = parser.parse_args()
-    path = args.path
-    video_name = args.video
-    output_file = args.output
-    metadata_file = args.metadata
-    short_video_length = args.short_video_length
-    profile_length = args.profile_length
-    offset = args.offset
-    target_f1 = args.target_f1
-    img_name_format = args.format
+    f_trace = open(args.trace_path+'/{}_trace.csv'.format(args.video), 'w')
+    f_trace.write('frame id,timestamp,trigger\n')
+    tstamp = 0
 
-    para1_dict = {}
-    with open('glimpse_perfect_tracking.csv', 'r') as f:
-        f.readline()
-        for line in f:
-            cols = line.strip().split(',')
-            para1_dict[cols[0]] = float(cols[1])
+    det_file = os.path.join(DET_ROOT, args.video, '720p',
+                            'profile/updated_gt_FasterRCNN_COCO_no_filter.csv')
+    img_path = os.path.join(IMG_ROOT, args.video, '720p')
+    video = YoutubeVideo(args.video, '720p', args.metadata, det_file, img_path,
+                         filter_flag=True, merge_label_flag=True)
 
-    with open(
-        '/home/zxxia/benchmarking/feature_analysis/selected_videos.json',
-            'r') as f:
-        selected_videos = json.load(f)
-    interested_videos = []
-    for l in selected_videos.values():
-        interested_videos.extend(l)
+    pipeline = Glimpse(PARA1_LIST_DICT[args.video], PARA2_LIST, args.log,
+                       args.profile_trace_path)  # mode='tracking'
+
     # choose the first 3 mins to get the best frame diff thresh
-    with open(output_file, 'w', 1) as final_result_f:
-        header = 'video,para1,para2,f1,gpu,ideal frame rate,trigger f1\n'
-        final_result_f.write(header)
-        # for video_type in DATASET_LIST:
-        f_profile = open('tmp_profile_E2E/profile_{}.csv'.format(video_name), 'w')
-        metadata = load_metadata(metadata_file)
-        resolution = metadata['resolution']
-        fps = metadata['frame rate']
-        # frame_count = metadata['frame count']
+    with open(args.output, 'w', 1) as final_result_f:
+        result_writer = csv.writer(final_result_f)
+        header = ['video chunk', 'para1', 'para2', 'f1',
+                  'frame rate', 'frame diff fps', 'tracking fps']
+        result_writer.writerow(header)
 
-        # resize the current video frames to 10Hz
         # read ground truth and full model detection result
         # image name, detection result, ground truth
 
-        img_path = path + str(resolution[1]) + 'p/'
-        # img_path = path + '720p/'
-        f_video_log = open('profile_E2E/profile_{}.json'.format(video_name),
-                           'w')
-        annot_file = img_path + 'profile/updated_gt_FasterRCNN_COCO.csv'
-        gt_annot, frame_end = load_full_model_detection(annot_file)
+        nb_short_videos = (video.frame_count - args.offset *
+                           video.frame_rate) // (args.short_video_length *
+                                                 video.frame_rate)
 
-        num_of_short_videos = (frame_end-offset*fps)//(short_video_length*fps)
+        for i in range(nb_short_videos):
+            start = i * args.short_video_length * video.frame_rate + \
+                video.start_frame_index + args.offset*video.frame_rate
+            end = (i+1)*args.short_video_length * \
+                video.frame_rate+args.offset*video.frame_rate
 
-        video_log = {}
-        for i in range(num_of_short_videos):
-            start = i * (short_video_length*fps) + 1 + offset * fps
-            end = (i+1)*(short_video_length*fps) + offset * fps
-            clip = video_name + '_' + str(i)
-            if clip not in interested_videos:
-                continue
+            clip = args.video + '_' + str(i)
 
             profile_start = start
-            profile_end = min(start+profile_length*fps-1, end)
+            profile_end = start+args.profile_length * video.frame_rate-1
+            test_start = profile_end + 1
+            test_end = end
             print("{} {} start={}, end={}".format(clip, img_path, start, end))
-            # print(video_type, seg_index)
-            # Run inference on the first 30s video
-            min_f1_gt_target = 1.0
-            # the minimum f1 score which is greater than
-            # or equal to target f1(e.g. 0.9)
-            min_fps = fps
+            best_para1, best_para2 = pipeline.profile(clip, video,
+                                                      profile_start,
+                                                      profile_end)
 
-            best_para1 = -1
-            best_para2 = -1
-            best_trigger_f1 = 0
-            profiling_logs = list()
+            print("best_para1={}, best_para2={}".format(best_para1,
+                                                        best_para2))
 
-            # for para1 in PARA1_LIST:
-            for para1 in [para1_dict[clip]]:
-                for para2 in PARA2_LIST:
-                    # larger para1, smaller thresh, easier to be triggered
-                    frame_diff_th = resolution[0]*resolution[1]/para1
-                    tracking_err_th = para2
-                    # images start from index 1
-                    print(img_path, profile_start, profile_end)
-                    triggered_frame, ideal_triggered_frame, f1, trigger_f1, \
-                        frames_log = pipeline(img_path, gt_annot,
-                                              profile_start, profile_end,
-                                              resolution, frame_diff_th,
-                                              tracking_err_th, img_name_format,
-                                              display)
+            ideal_triggered_frame, f1, trigger_f1, pix_change_obj, \
+                pix_change_bg, frame_diff_triggered, tracking_triggered, \
+                frames_log = pipeline.evaluate(video, test_start, test_end,
+                                               best_para1, best_para2)
+            # use the selected parameters for the next 5 mins
+            frames_triggered = frame_diff_triggered.union(tracking_triggered)
+            final_fps = len(frames_triggered) / (test_end - test_start + 1)
+            frame_diff_fps = len(frame_diff_triggered) / \
+                (test_end - test_start + 1)
+            tracking_fps = len(tracking_triggered) / \
+                (test_end - test_start + 1)
+            result_writer.writerow(
+                [clip, best_para1, best_para2, f1, final_fps, frame_diff_fps,
+                 tracking_fps])
 
-                    current_fps = triggered_frame/profile_length
-                    ideal_fps = ideal_triggered_frame/profile_length
-                    profiling_log = {'para1': para1,
-                                     'para2': para2,
-                                     'frame difference threshold':
-                                     float(frame_diff_th),
-                                     'tracking error threshold':
-                                     float(tracking_err_th),
-                                     'frames log': frames_log,
-                                     'processing time': float(current_fps/fps),
-                                     'ideal processing time':
-                                     float(ideal_fps/fps),
-                                     'f1': float(f1),
-                                     'frame difference trigger f1':
-                                     float(trigger_f1)}
-                    profiling_logs.append(profiling_log)
-                    print('para1 = {}, para2 = {}, '
-                          'Profiled f1 = {}, Profiled perf = {}, Ideal perf={}'
-                          .format(para1, para2, f1, current_fps/fps,
-                                  ideal_fps/fps))
-                    f_profile.write(','.join([clip, str(para1), str(para2),
-                                              str(f1), str(current_fps/fps),
-                                              str(ideal_fps/fps)])+'\n')
-                    if f1 >= target_f1:
-                        break
-
-                if f1 >= target_f1 \
-                   and f1 <= min_f1_gt_target \
-                   and current_fps < min_fps:
-                    min_f1_gt_target = f1
-                    min_fps = current_fps
-                    # record the best config
-                    best_para1 = para1
-                    best_para2 = para2
-                    best_trigger_f1 = trigger_f1
-                    break
-            video_log[clip] = profiling_logs
-            print("best_para1 = {}, best_para2={}"
-                  .format(best_para1, best_para2))
-            final_result_f.write(','.join([clip, str(best_para1),
-                                           str(best_para2),
-                                           str(min_f1_gt_target),
-                                           str(min_fps/fps),
-                                           str(ideal_fps/fps),
-                                           str(best_trigger_f1)])+'\n')
-            json.dump(video_log, f_video_log, sort_keys=True, indent=4)
-
-        f_profile.close()
-        f_video_log.close()
+            frames_log_file = os.path.join(
+                args.trace_path,
+                clip + '_{}_{}_frames_log.csv'.format(best_para1, best_para2))
+            with open(frames_log_file, 'w') as f:
+                frames_log_writer = csv.DictWriter(
+                    f, ['frame id', 'frame diff', 'frame diff thresh',
+                        'frame diff trigger', 'tracking error',
+                        'tracking error thresh', 'tracking trigger',
+                        'detection'])
+                frames_log_writer.writeheader()
+                frames_log_writer.writerows(frames_log)
+            for idx in range(start, end + 1):
+                if idx in set(range(profile_start, profile_end + 1))\
+                        .union(frames_triggered):
+                    f_trace.write(
+                        ','.join([str(idx), str(tstamp), str(1)])+'\n')
+                else:
+                    f_trace.write(
+                        ','.join([str(idx), str(tstamp), str(0)]) + '\n')
+                tstamp += 1/video.frame_rate
+        f_trace.close()
 
 
 if __name__ == '__main__':
--- video.py
+++ video.py
@@ -1,5 +1,6 @@
 """Definition of videos."""
 import copy
+import configparser
 import os
 import pdb
 import subprocess
@@ -372,6 +373,40 @@ class WaymoVideo(Video):
 
 
 class MOT16Video(Video):
+    """Class of MOT16Video."""
+
+    def __init__(self, root, video_name, resolution_name,
+                 detection_file, image_path,
+                 model='FasterRCNN', filter_flag=True, merge_label_flag=False):
+        """MOT16Video Constructor."""
+        dets, num_of_frames = load_full_model_detection(detection_file)
+        dets_nofilter = copy.deepcopy(dets)
+        # resolution = (1242, 375)
+        resolution = RESOL_DICT[resolution_name]
+        config = configparser.ConfigParser()
+        seqinfo_path = os.path.join(root, 'seqinfo.ini')
+        config.read(seqinfo_path)
+        frame_rate = config['Sequence']['frameRate']
+        if filter_flag:
+            dets, dropped_dets = filter_video_detections(
+                dets,
+                target_types={COCOLabels.CAR.value,
+                              COCOLabels.BUS.value,
+                              COCOLabels.TRUCK.value},
+                height_range=(resolution[1] // 20, resolution[1]))
+            self._dropped_detections = dropped_dets
+            if merge_label_flag:
+                for frame_idx, boxes in dets.items():
+                    for box_idx, _ in enumerate(boxes):
+                        # Merge all cars and trucks into cars
+                        dets[frame_idx][box_idx][4] = COCOLabels.CAR.value
+                #     dets[frame_idx] = remove_overlappings(boxes, 0.3)
+        else:
+            dropped_dets = None
+        super().__init__(video_name, frame_rate, resolution, dets,
+                         dets_nofilter, image_path, 'moving', model,
+                         dropped_dets)
+
     @staticmethod
     def convert_grondtruth(gt_file):
         gt = pd.read_csv(gt_file)
