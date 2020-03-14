e8fec4d29363c5e57d276b38eaaf1c890bd94b28
Zhengxu Xia <kevinxzx95@gmail.com>
2020-03-13 20:39:09 -0500

    rename inference model and create a wrapper over tensorflow module.

--- object_detection/infer.py
+++ object_detection/infer.py
@@ -1,4 +1,4 @@
-"""Object detection module."""
+
 import csv
 import glob
 import os
@@ -12,68 +12,97 @@ from PIL import Image
 
 from benchmarking.utils.utils import nms
 
-
-def load_model(model_dir):
-    """Load model downloaded from tensorflow model zoo."""
-    model_dir = os.path.join(model_dir, "saved_model")
-    model = tf.saved_model.load(str(model_dir))
-    model = model.signatures['serving_default']
-    return model
+from model import Model
 
 
-def run_inference_for_single_image(model, image):
-    """Run object detection on a single image."""
-    image = np.asarray(image)
-    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-    input_tensor = tf.convert_to_tensor(image)
-    # The model expects a batch of images, so add an axis with `tf.newaxis`.
-    input_tensor = input_tensor[tf.newaxis, ...]
-
-    # Run inference
-    output_dict = model(input_tensor)
-
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and
-    # take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
-    num_detections = int(output_dict.pop('num_detections'))
-    output_dict = {key: value[0, :num_detections].numpy()
-                   for key, value in output_dict.items()}
-    output_dict['num_detections'] = num_detections
-
-    # detection_classes should be ints.
-    output_dict['detection_classes'] = output_dict['detection_classes'].astype(
-        np.int64)
-
-    return output_dict
-
-
-def write_to_file(csvwriter, img_path, resolution, detections):
+def write_to_file(csvwriter, img_path, resolution, detections, profile_writer,
+                  t_used):
     """Write detetions into a csv file."""
-    for label, box, score in zip(detections['detection_classes'],
-                                 detections['detection_boxes'],
-                                 detections['detection_scores']):
-        csvwriter.writerow(
-            [int(os.path.basename(os.path.splitext(img_path)[0])),
-             box[1]*resolution[0], box[0]*resolution[1],
-             box[3]*resolution[0], box[2]*resolution[1], label, score])
+    # pdb.set_trace()
+    frame_id = int(os.path.basename(os.path.splitext(img_path)[0]))
+    if detections['num_detections'] == 0:
+        csvwriter.writerow([frame_id, '', '', '', '', '', ''])
+    else:
+        for label, box, score in zip(detections['detection_classes'],
+                                     detections['detection_boxes'],
+                                     detections['detection_scores']):
+            csvwriter.writerow(
+                [frame_id,
+                 box[1]*resolution[1], box[0]*resolution[0],
+                 box[3]*resolution[1], box[2]*resolution[0], label, score])
+    profile_writer.writerow([frame_id, t_used])
+
+
+def infer(args):
+    """Do object detection."""
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    assert len(gpus) > 0, "Not enough GPU hardware devices available"
+    tf.config.experimental.set_visible_devices(gpus[args.device], 'GPU')
+    tf.config.experimental.set_memory_growth(gpus[args.device], True)
+    model = Model(args.model)
+    model_name = os.path.basename(args.model)
+
+    dets_path = os.path.join(args.output_path, f'{model_name}_detections.csv')
+    profile_path = os.path.join(args.output_path, f'{model_name}_profile.csv')
+    with open(dets_path, 'w', 1) as f, open(profile_path, 'w', 1) as f_profile:
+        writer = csv.writer(f)
+        writer.writerow(['frame id', 'xmin', 'ymin',
+                         'xmax', 'ymax', 'class', 'score'])
+        profile_writer = csv.writer(f_profile)
+        profile_writer.writerow(['frame id', 'time used(s)'])
+        img_paths = sorted(glob.glob(os.path.join(args.input_path, '*.jpg')))
+        for i, img_path in enumerate(img_paths):
+            image = np.array(Image.open(img_path))
+            detections, t_used = model.infer_single_image(image)
+            write_to_file(writer, img_path, image.shape,
+                          detections, profile_writer, t_used)
+            if (i+1) % 10 == 0 or (i+1) == len(img_paths):
+                print('Processed {}/{} images...'.format(i+1, len(img_paths)))
+
+        img_paths = sorted(glob.glob(os.path.join(args.input_path, '*.png')))
+        for i, img_path in enumerate(img_paths):
+            image = np.array(Image.open(img_path))
+            detections, t_used = model.infer_single_image(image)
+            write_to_file(writer, img_path, image.shape,
+                          detections, profile_writer, t_used)
+            if (i+1) % 10 == 0 or (i+1) == len(img_paths):
+                print('Processed {}/{} images...'.format(i+1, len(img_paths)))
+
+    dets = load_object_detection_results(dets_path)
+    dets = smooth_annot(dets)
+    dets = tag_object(dets)
+    smoothed_dets_path = os.path.join(
+        args.output_path, f'{model_name}_smoothed_detections.csv')
+    with open(smoothed_dets_path, 'w', 1) as f:
+        writer = csv.writer(f)
+        writer.writerow(['frame id', 'xmin', 'ymin',
+                         'xmax', 'ymax', 'class', 'score', 'object id'])
+        for frame_id in sorted(dets):
+            for box in dets[frame_id]:
+                writer.writerow([frame_id]+box)
 
 
 def load_object_detection_results(filename):
     """Load object detection results.
 
-    filename(string): filename of object detection results in csv format.
-    return(dict): a dict mapping frame id to ['xmin', 'ymin', 'xmax', 'ymax',
-                                              'class', 'score', 'object id']
+    Args
+        filename(string): filename of object detection results in csv format.
+    Return
+        dets(dict): a dict mapping frame id to bounding boxes.
+
     """
     df = pd.read_csv(filename)
+    dets = {}
     if 'object id' in df.columns:
-        dets = {k: g[['xmin', 'ymin', 'xmax', 'ymax', 'class',
-                      'score', 'object id']
-                     ].values.tolist() for k, g in df.groupby("frame id")}
+        value_names = ['xmin', 'ymin', 'xmax', 'ymax',
+                       'class', 'score', 'object id']
     else:
-        dets = {k: g[['xmin', 'ymin', 'xmax', 'ymax', 'class', 'score']
-                     ].values.tolist() for k, g in df.groupby("frame id")}
+        value_names = ['xmin', 'ymin', 'xmax', 'ymax', 'class', 'score']
+    for k, g in df.groupby("frame id"):
+        if g.isna().any().any():
+            dets[k] = []
+        else:
+            dets[k] = g[value_names].values.tolist()
     return dets
 
 
@@ -248,46 +277,3 @@ def smooth_annot(dets, dist_coef=0.45):
                 dets[frame_id] = current_boxes
 
     return dets
-
-
-def infer(args):
-    """Do object detection."""
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    assert len(gpus) > 0, "Not enough GPU hardware devices available"
-    tf.config.experimental.set_visible_devices(gpus[args.device], 'GPU')
-    tf.config.experimental.set_memory_growth(gpus[args.device], True)
-    model = load_model(args.model)
-
-    dets_path = os.path.join(args.output_path, 'detections.csv')
-    with open(dets_path, 'w', 1) as f:
-        writer = csv.writer(f)
-        writer.writerow(['frame id', 'xmin', 'ymin',
-                         'xmax', 'ymax', 'class', 'score'])
-        img_paths = sorted(glob.glob(os.path.join(args.input_path, '*.jpg')))
-        for i, img_path in enumerate(img_paths):
-            image = np.array(Image.open(img_path))
-            detections = run_inference_for_single_image(model, image)
-            write_to_file(writer, img_path, image.shape, detections)
-            if (i+1) % 10 == 0 or (i+1) == len(img_paths):
-                print('Processed {}/{} images...'.format(i+1, len(img_paths)))
-
-        img_paths = sorted(glob.glob(os.path.join(args.input_path, '*.png')))
-        for i, img_path in enumerate(img_paths):
-            image = np.array(Image.open(img_path))
-            detections = run_inference_for_single_image(model, image)
-            write_to_file(writer, img_path, image.shape, detections)
-            if (i+1) % 10 == 0 or (i+1) == len(img_paths):
-                print('Processed {}/{} images...'.format(i+1, len(img_paths)))
-
-    dets = load_object_detection_results(dets_path)
-    dets = smooth_annot(dets)
-    dets = tag_object(dets)
-    smoothed_dets_path = os.path.join(
-        args.output_path, 'smoothed_detections.csv')
-    with open(smoothed_dets_path, 'w', 1) as f:
-        writer = csv.writer(f)
-        writer.writerow(['frame id', 'xmin', 'ymin',
-                         'xmax', 'ymax', 'class', 'score', 'object id'])
-        for frame_id in sorted(dets):
-            for box in dets[frame_id]:
-                writer.writerow([frame_id]+box)
