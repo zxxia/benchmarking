5c2326d63360b839eb6ee1b7203efd9caf55f066
Zhengxu Xia <kevinxzx95@gmail.com>
2020-07-01 15:29:25 -0500

    add todo in profiler and performances estimator.

---
+++ performance_estimator/estimate.py
@@ -0,0 +1,8 @@
+
+def estimate(pipeline, configuration, new_video, profiles):
+    # TODO: compute features of the new video.
+    # TODO: load profiles and look up performance from the profiles
+    # TODO: compute e2e performance under this configuration
+    raise NotImplementedError
+    estimated_perf = None
+    return estimated_perf
---
+++ profiler/profile.py
@@ -0,0 +1,7 @@
+def profile(pipeline, configurations, selected_videos):
+    # TODO: compute features of the selected videos.
+    # TODO: iterate all configurations and measure E2E performance
+    # TODO: iterate all configurations and measure each primitive performance
+    raise NotImplementedError
+    feauture_vs_perf = None
+    return feauture_vs_perf
