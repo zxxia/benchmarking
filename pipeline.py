e1d27c4344f0ba8248f0b4b7b629f66d7d48e869
Zhengxu Xia <kevinxzx95@gmail.com>
2020-07-02 10:53:02 -0500

    add few comments

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,6 @@
 class Pipeline(object):
     def __init__(self):
+        # TODO: need to contain temporal, spatial, and model pruning
+        # TODO: need to contain configurations
+
         raise NotImplementedError("Pipeline base class not implemented.")
--- profile.py
+++ profile.py
@@ -1,3 +1,6 @@
 class Profile(object):
     def __init__(self):
+
+        # TODO: read from csv/json files
+        # TODO: need to contain temporal, spatial, and model pruning profiles
         raise NotImplementedError("Profile base class not implemented.")
