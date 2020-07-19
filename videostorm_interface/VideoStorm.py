de83612746a0d5869a21ea948d6dda683d736280
ChampionNan <championnan@foxmail.com>
2020-07-20 01:29:14 +0800

    Finishing Server & evaluate

--- videostorm_interface/VideoStorm.py
+++ videostorm_interface/VideoStorm.py
@@ -1,9 +1,11 @@
 import copy
 import csv
+import sys
 from collections import defaultdict
 
 from constants import MODEL_COST
 from utils.utils import interpolation
+from videos import get_dataset_class, get_seg_paths
 from evaluation.f1 import compute_f1, evaluate_frame
 
 from interface import Temporal, Spatial, Model
@@ -11,10 +13,91 @@ from pipeline import Pipeline
 
 
 class VideoStorm_Temporal(Temporal):
+    '''use sample rate'''
+    def __init__(self, temporal_sampling_list, videostorm_temporal_flag):
+        '''
+        :param temporal_sampling_list: flag == 1 -> sampling_list regard as sample rate; else: sample_rate = 0
+        :param videostorm_temporal_flag: flag whether use videostorm_tempora pruning
+        '''
+        if videostorm_temporal_flag:
+            # use input sample
+            self.temporal_sampling_list = temporal_sampling_list
+        else:
+            # no sampling
+            self.temporal_sampling_list = [sys.maxsize]
+
+    def run(self, segment, decision, results):
+        pass
+
 
 class VideoStorm_Spacial(Spatial):
+    '''get certain resolution video'''
+    def __init__(self, original_resolution, spacial_resolution, videostorm_spacial_flag):
+        if videostorm_spacial_flag:
+            self.resolution = spacial_resolution
+        else:
+            self.resolution = original_resolution
+
+    def run(self, segment, decision, results):
+        pass
+
 
 class VideoStorm_Model(Model):
+    '''use different model'''
+    def __init__(self, model_list, videostorm_spacial_flag):
+        if videostorm_spacial_flag:
+            self.model_list = model_list
+        else:
+            self.model_list = ['FasterRCNN', 'inception', 'mobilenet']
+
+    def run(self, segment, decision, results):
+        pass
+
 
 class VideoStorm(Pipeline):
+    def __init__(self, temporal_sampling_list, model_list, original_resolution, spacial_resolution, profile_log, target_f1 = 0.9, videostorm_temporal_flag = 0, videostorm_spacial_flag = 0, videostorm_model_flag = 0):
+        '''
+        Load the configs and initialize VideoStorm_interface pipeline.
+        :param temporal_sampling_list: a list of sample rates
+        :param model_list: a list of model names
+        :param resolution: selected resolution
+        :param target_f1: target f1 score
+        :param temporal_prune: Temporal prune instance for Videostorm
+        :param spacial_prune: Spacial prune instance for Videostorm
+        :param model_prune: Model prune instance for Videostorm
+        '''
+        self.target_f1 = target_f1
+        self.profile_writer = csv.writer(open(profile_log, 'w', 1))
+        # Add header
+        self.profile_writer.writerow(['video_name', 'model', 'frame_rate', 'gpu time', 'f1'])
+        # pruning flags
+        self.videostorm_temporal_flag = videostorm_temporal_flag
+        self.videostorm_spacial_flag = videostorm_spacial_flag
+        self.videostorm_model_flag = videostorm_model_flag
+        # use these to get temporal_sampling_list, model_list, resolution
+        self.videostorm_temporal = VideoStorm_Temporal(temporal_sampling_list, videostorm_temporal_flag)
+        self.videostorm_spacial = VideoStorm_Spacial(original_resolution, spacial_resolution, videostorm_spacial_flag)
+        self.videostorm_model = VideoStorm_Model(model_list, videostorm_model_flag)
 
+
+    def Source(self, clip, pruned_video_dict, original_video, frame_range):
+        '''
+        :param clip: video_frame
+        :param pruned_video_dict: videos after pruned
+        :param original_video: groundtruth
+        :param frame_range: video frame range
+        :return: best_frame_rate, best_model
+        '''
+        self.Server(clip, pruned_video_dict, original_video, frame_range)
+
+
+    def Server(self, clip, pruned_video_dict, original_video, frame_range):
+        '''
+        :param the same as Source params
+        '''
+
+
+
+
+
+    def evaluate(self, video, original_video, sample_rate, frame_range):
--- videostorm_interface/run.py
+++ videostorm_interface/run.py
@@ -8,5 +8,76 @@ from videostorm_interface.VideoStorm import VideoStorm, VideoStorm_Temporal, Vid
 def run(args):
     """Run VideoStorm simulation."""
     dataset_class = get_dataset_class(args.dataset)
-    seg_path = get_seg_paths(args.data_root, args.dataset, args.vedio)
-    original_resolution = 
+    seg_paths = get_seg_paths(args.data_root, args.dataset, args.vedio)
+    original_resolution = args.original_resolution
+    overfitting = args.overfitting
+    model_list = args.module_list if not overfitting else ['fastre_rcnn_resnet101']
+    sample_step_list = args.sample_step_list
+    short_video_length = args.short_video_length
+    profile_length = args.profile_length
+    profile_filename = args.profile_filename
+    output_filename = args.output_filename
+    videostorm_temporal_flag = args.videostorm_temporal_flag
+    videostorm_spacial_flag = args.videostorm_spacial_flag
+    videostorm_model_flag = args.videostorm_model_flag
+    spacial_resolution = args.spacial_resolution
+
+    coco_id2name, coco_name2id = load_COCOlabelmap(args.coco_label_file)
+    classes_interested = {coco_name2id[class_type] for class_type in args.classes_interested}
+    if overfitting:
+        assert short_video_length == profile_length, "short_video_length should equal to profile_length when overfitting."
+    else:
+        assert short_video_length >= profile_length, "short_video_length should no less than profile_length."
+
+    # videostorm_temporal = VideoStorm_Temporal(sample_step_list, )
+    # videostorm_spacial = VideoStorm_Spacial(original_resolution, dataset_class)
+    # videostorm_model = VideoStorm_Model(model_list)
+    pipeline = VideoStorm(sample_step_list, model_list, original_resolution, spacial_resolution, profile_filename, videostorm_temporal_flag, videostorm_spacial_flag, videostorm_model_flag)
+    with open(output_filename, 'w', 1) as f_out:
+        writer = csv.writer(f_out)
+        writer.writerow(['video_name', 'model', 'gpu time', 'frame_rate', 'f1'])
+        for seg_path in seg_paths:
+            seg_name = os.path.basename(seg_path)
+            original_video = dataset_class(seg_path, seg_name, original_resolution, 'faster_rcnn_resnet101', filter_flag=true, classes_interested=classes_interested)
+            videos = {}
+            for model in pipeline.videostorm_model.model_list:
+                video = dataset_class(seg_path, seg_name, pipeline.videostorm_spacial.resolution, model, filter_flag=true, classes_interested=classes_interested)
+                videos[model] = video
+
+            if original_video.duration > short_video_length:
+                nb_short_videos = original_video.frame_count // (short_video_length * original_video.frame_rate)
+            else:
+                nb_short_videos = 1
+
+            # equals: while seg = Next_segment(video):
+            for i in range(nb_short_videos):
+                clip = seg_name + '_' + str(i)
+                start_frame = i * short_video_length * original_video.frame_rate + original_video.start_frame_index
+                end_frame = min((i + 1) * short_video_length * original_video.frame_rate, original_video.end_frame_index)
+                print('{} start={} end={}'.format(clip, start_frame, end_frame))
+
+                if overfitting:
+                    profile_start = start_frame
+                    profile_end = end_frame
+                else:
+                    profile_start = start_frame
+                    profile_end = start_frame + original_video.frame_rate * profile_length - 1
+                print('profile {} start={} end={}'.format(clip, profile_start, profile_end))
+                best_frame_rate, best_model = pipeline.Source(clip, videos, original_video, [profile_start, profile_end])
+                best_sample_rate = original_video.frame_rate / best_frame_rate
+
+                if overfitting:
+                    test_start = start_frame
+                    test_end = end_frame
+                else:
+                    test_start = profile_end + 1
+                    test_end = end_frame
+
+                print('Evaluate {} start={} end={}'.format(clip, test_start, test_end))
+                f1_score, relative_gpu_time, triggered_frame_tmp = pipeline.evaluate(videos[best_model], original_video, best_sample_rate, [test_start, test_end])
+                print('Evaluate each frame result: clip={}, best_model={}, relative_gpu_time={}, best_frame_rate / original_video.frame_rate={}, f1_score={}'.format(clip, best_model, relative_gpu_time, best_frame_rate / original_video.frame_rate, f1_score))
+                writer.writerow([clip, best_model, relative_gpu_time, best_frame_rate / original_video.frame_rate, f1_score])
+
+
+
+
